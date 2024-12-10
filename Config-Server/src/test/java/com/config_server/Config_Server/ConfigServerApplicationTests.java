348e052921e7ecad4b7eca8799f9f28bc0b6d3f2
Pedro Leonardo Migliorini <leonardomigliorini2@gmail.com>
2024-12-10 19:14:53 -0300

    applications.yml configurations

---
+++ Config-Server/src/test/java/com/config_server/Config_Server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.config_server.Config_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Gateway/src/test/java/com/ApiGateway/Gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApiGateway.Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
